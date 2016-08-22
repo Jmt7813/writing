7a33e56109c82e44bfb81259d8fec37b10571341
Jmt7813 <jmt1183@gmail.com>
2016-08-22 16:48:45 -0700

    Created john.py

---
+++ john.py
@@ -0,0 +1,3 @@
+print "This is the latest and greatest information from John Thomason"
+print "I don't really know what the hell is happening here, but I do know that I don't care so much
+print "Maybe I should have remembered to put another quote on the end of that last line" It might have really helped out.
